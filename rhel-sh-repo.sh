12b7cb1033c08b85fc0b8e6e6a49847cb91e8a8d
Irwan Setiawan <irwanscreative@gmail.com>
2014-08-30 20:03:19 +0800

    Update rhel-sh-repo.sh

--- rhel-sh-repo.sh
+++ rhel-sh-repo.sh
@@ -2,12 +2,12 @@
 
 # EPEL Repository
 echo "Adding EPEL Repository"
-rpm -ivh http://download.fedoraproject.org/pub/epel/6/$(arch)/epel-release-6-8.noarch.rpm
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/$(arch)/epel-release-6-8.noarch.rpm
 
 # Remi Repository
 echo "Adding remi Repository"
-rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
 # rpmforge Repository
 echo "Adding rpmforge Repository"
-rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
