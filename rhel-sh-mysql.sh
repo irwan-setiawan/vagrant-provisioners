175a91dd497cf59ee92006938025c58e468ff6e2
Irwan Setiawan <irwanscreative@gmail.com>
2014-08-30 18:46:11 +0800

    Create rhel-sh-mysql.sh

---
+++ rhel-sh-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install MySQL, if not already installed
+if ! rpm -qa | grep -qw "mysql-server"; then
+	echo "Installing MySQL Server"
+	yum install -q -y mysql-server
+else
+	echo "Checking MySQL Server.. Already Installed"
+fi
+
+# Register MySQL as a service
+echo "Registering MySQL as a service"
+chkconfig mysqld on
+
+# Start MySQL service
+echo "Starting MySQL Service"
+service mysqld restart
