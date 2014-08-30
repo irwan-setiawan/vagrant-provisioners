89479accac12e35f5a1ea0fc747201f96536d789
Irwan Setiawan <irwanscreative@gmail.com>
2014-08-30 19:37:44 +0800

    Create rhel-sh-laravel.sh

---
+++ rhel-sh-laravel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Set DocumentRoot in Apache config file to the project files where it is shared in /vagrant
+echo "Setting Apache's DocumentRoot for Laravel"
+sed -i 's/^DocumentRoot .*/DocumentRoot "\/vagrant\/public"/g' /etc/httpd/conf/httpd.conf
+sed -i 's/\/var\/www\/html/\/vagrant\/public/g' /etc/httpd/conf/httpd.conf
