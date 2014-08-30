1edff5dd176fd4cb56d906f8c6c369d393dc08ea
Irwan Setiawan <irwanscreative@gmail.com>
2014-08-30 22:53:22 +0800

    Update rhel-sh-mysql.sh

--- rhel-sh-mysql.sh
+++ rhel-sh-mysql.sh
@@ -8,6 +8,15 @@ else
 	echo "Checking MySQL Server.. Already Installed"
 fi
 
+
+# Install mysql-libs, if not already installed
+if ! rpm -qa | grep -qw "mysql-libs"; then
+	echo "Installing MySQL-libs"
+	yum install -q -y mysql-libs
+else
+	echo "Checking MySQL-libs.. Already Installed"
+fi
+
 # Register MySQL as a service
 echo "Registering MySQL as a service"
 chkconfig mysqld on
