4679c7159f0283c7b505f1ed726e7b938d319b5f
Irwan Setiawan <irwanscreative@gmail.com>
2014-08-30 23:08:38 +0800

    Update rhel-sh-laravel.sh

--- rhel-sh-laravel.sh
+++ rhel-sh-laravel.sh
@@ -8,3 +8,12 @@ sed -i 's/\/var\/www\/html/\/vagrant\/public/g' /etc/httpd/conf/httpd.conf
 # Restart Apache service
 echo "Restarting Apache Service"
 service httpd restart
+
+
+# Install php-mcrypt, if not already installed
+if ! rpm -qa | grep -qw "php-mcrypt"; then
+	echo "Installing php-mcrypt"
+	yum install -q -y php-mcrypt
+else
+	echo "Checking php-mcrypt.. Already Installed"
+fi
