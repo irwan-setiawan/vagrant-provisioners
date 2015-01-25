1d4e436b0bdabf9d79df446170fca94f4c1e5c5a
Irwan Setiawan <irwanscreative@gmail.com>
2015-01-25 17:27:53 +0800

    install EPEL repo from yum

--- rhel-sh-repo-epel.sh
+++ rhel-sh-repo-epel.sh
@@ -2,5 +2,4 @@
 
 # EPEL Repository
 echo "Adding EPEL Repository"
-rpm -ivh --force http://download.fedoraproject.org/pub/epel/6/$(arch)/epel-release-6-8.noarch.rpm
-sed -i '1,10 s/^enabled.*=.*/enabled=1/g' /etc/yum.repos.d/epel.repo
+yum install -q -y epel-release
